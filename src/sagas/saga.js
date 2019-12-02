bee29e6dfba05da7046f146a3c27ede8d64e40c9
LeoYan <leoyanisme@gmail.com>
2019-12-02 16:59:41 +0800

    upload saga_example

---
+++ src/sagas/saga.js
@@ -0,0 +1,11 @@
+// import { delay } from 'redux-saga';
+import { takeEvery, put, delay } from 'redux-saga/effects';
+
+function* ageUpAsync() {
+    yield delay(4000)
+    yield put({ type: 'AGE_UP_ASYNC', value: 1 })
+}
+
+export function* watchAgeUp() {
+    yield takeEvery('AGE_UP', ageUpAsync)
+}
